2d081ee9a70f494d44c1d074e280f9e0a1c01593
Nate Grigg <nate.grigg@modelop.com>
2025-05-06 16:57:39 -0600

    take a batch file as input

--- predict.py
+++ predict.py
@@ -8,3 +8,16 @@ example = [[5.1, 3.5, 1.4, 0.2]]
 pred = model.predict(example)
 
 print(f'Prediction: {pred}')
+
+if __name__ == '__main__':
+    iris_input = []
+    iris_output = []
+    with open('iris_input', 'r') as iris_input_file:
+        iris_input = iris_input_file.read().splitlines()
+
+    for input_line in iris_input:
+        iris_output.append(predict(input_line))
+        
+    with open('iris_output', 'w') as iris_output_file:
+        iris_output_file.writelines(iris_output)
+
