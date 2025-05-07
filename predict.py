cf8eb1b88c26ebf072dd3c5da4cf4f1f1ff76307
Nate Grigg <nate.grigg@modelop.com>
2025-05-07 10:09:41 -0600

    rename variable

--- predict.py
+++ predict.py
@@ -1,11 +1,12 @@
 import joblib
 
-# load the trained model
-model = joblib.load('iris_model.pkl')
+def begin():
+    # load the trained model
+iris_model = joblib.load('iris_model.pkl')
 
 # Example input (first iris sample)
 example = [[5.1, 3.5, 1.4, 0.2]]
-pred = model.predict(example)
+pred = iris_model.predict(example)
 
 print(f'Prediction: {pred}')
 
